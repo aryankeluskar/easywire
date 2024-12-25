e2aa7a423c3a6af98abb25d39c62f92130eb7a9c
Aryan Keluskar <aryankeluskar6@gmail.com>
2024-12-25 13:58:54 -0700

    bugfix vercel deployment

--- api.py
+++ api.py
@@ -1,22 +1,13 @@
+import json
 from typing import Annotated
-
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import pandas as pd
-
-from fastapi.templating import Jinja2Templates
-from math import floor
-
-import datetime
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
-
 load_dotenv()
-
-TEMPLATES = Jinja2Templates(directory="/")
 
 app = FastAPI()
 app.add_middleware(
@@ -28,20 +19,12 @@ app.add_middleware(
 )
 
 templates_dir = os.path.join(os.path.dirname(__file__), "templates", "homepage_files")
-print(templates_dir)
 app.mount(
     "/homepage_files",
     StaticFiles(
         directory=templates_dir,
     ),
     name="homepage_files",
-)
-app.mount(
-    "/templates",
-    StaticFiles(
-        directory=os.path.join(os.path.dirname(__file__), "templates"),
-    ),
-    name="templates",
 )
 app.mount(
     "/css",
@@ -53,16 +36,10 @@ app.mount(
 app.mount(
     "/data",
     StaticFiles(
-        directory=os.path.join(
-            os.path.dirname(__file__),
-            "data",
-        )
+        directory=templates_dir,
     ),
     name="data",
 )
-
-
-templates = Jinja2Templates(directory="/")
 
 
 @app.get("/")
@@ -79,89 +56,49 @@ async def root():
     """
     # print list of files in templates directory
     print(os.listdir())
-    return FileResponse("./docker/templates/home.html")
+    return FileResponse("templates/home.html")
 
 
-@app.get("/sucess", response_class=HTMLResponse)
-async def success_demo(request: Request):
-    # return FileResponse("./docker/templates/success_trial.html")
-    return {"success": True}
-
-
-@app.post("/success", response_class=HTMLResponse)
+@app.post("/data")
 async def data(
     amount: Annotated[str, Form()] = "",
     date: Annotated[str, Form()] = "",
     email: Annotated[str, Form()] = "",
-    request: Request = None,
 ):
     # print("data")
     print("amount: " + amount)
     print("date: " + date)
     print("email: " + email)
-
-    _res = [0, "2024-09-31T00:00:00.000000000"]
-    savings = _res[0]
-    opt_date = _res[1]
-
-    print("opt_date: " + opt_date)
-    opt_date = opt_date[2:12]
-    print("opt_date: " + opt_date)
-
-    # opt_date is now '2024-09-20T00:00:00.000000000' convert to words like '20th September 2024'
-    opt_date = datetime.datetime.strptime(opt_date, "%Y-%m-%d")
-    opt_date = opt_date.strftime("%d %B %Y")
-
-    savings = round(savings, 3)
-
-    print("savings: " + str(savings))
-    print("optimal date: " + str(opt_date))
-
-    return TEMPLATES.TemplateResponse(
-        name="/success.html",
-        context={
-            "request": request,
-            "savings": savings,
-            "opt_date": opt_date,
-        },
-    )
-
-    # return ("../success.html?savings=" + str(savings) + "&opt_date=" + str(opt_date))
-
-@app.get("/graph/prediction")
-async def graph_prediction():
-    return FileResponse("./docker/data/prediction.png")
+    return {"success": True}
 
 @app.get("/graph/usd_inr_all")
 async def graph_usd_inr_all():
-    return FileResponse("./docker/data/usd_inr_all.png")
+    return FileResponse("data/usd_inr_all.png")
 
-    # import pandas as pd
-    # from prophet import Prophet
+    import pandas as pd
+    from prophet import Prophet
+    df = pd.read_csv('data/usd_inr.csv')
+    print("retrieved usd to inr data")
+    df.columns = ['ds', 'y']
 
-    # df = pd.read_csv("data/usd_inr.csv")
-    # print("retrieved usd to inr data")
-    # df.columns = ["ds", "y"]
+    # remove any NaN or other values
+    df = df.dropna()
 
-    # # remove any NaN or other values
-    # df = df.dropna()
+    # remove values in Y where there is a .
+    df = df[df['y'] != '.']
+    df.head()
+    
+    import matplotlib.pyplot as plt
+    plt.plot(df['ds'], df['y'])
+    print("retrieved usd to inr data")
 
-    # # remove values in Y where there is a .
-    # df = df[df["y"] != "."]
-    # df.head()
+    m = Prophet()
+    m.fit(df)
 
-    # import matplotlib.pyplot as plt
-
-    # plt.plot(df["ds"], df["y"])
-    # print("retrieved usd to inr data")
-
-    # m = Prophet()
-    # m.fit(df)
-
-    # future = m.make_future_dataframe(periods=365)
-    # forecast = m.predict(future)
-    # fig2 = m.plot_components(forecast)
-    # print(fig2)
-    # print(type(fig2))
-    # fig2.savefig("data/usd_inr_all.png")
-    # return FileResponse("data/usd_inr_all.png")
+    future = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future)
+    fig2 = m.plot_components(forecast)
+    print(fig2)
+    print(type(fig2))
+    fig2.savefig('data/usd_inr_all.png')
+    return FileResponse("data/usd_inr_all.png")
